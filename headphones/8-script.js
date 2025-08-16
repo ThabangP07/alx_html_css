5de7cf28e37edc02db7d8abf5a8aebfdaa228849
Thabang <phoshokothabang1@gmail.com>
2025-08-16 09:54:13 +0200

    feat: add an active state to the event listener for hamburger

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -4,5 +4,6 @@ const navLinks = document.getElementById('nav-links');
 
 // Toggle the menu on hamburger click
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active'); // Toggle the 'active' class on hamburger
     navLinks.classList.toggle('show'); // Toggle the 'show' class
 });
